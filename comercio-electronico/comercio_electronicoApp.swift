51c5166a9e1eb29ca3b5c9ca3ec77fd9905d0e4b
macOs <macos@10.0.2.15>
2025-05-21 14:05:41 -0700

    Primer commit para app con IOS

---
+++ comercio-electronico/comercio_electronicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  comercio_electronicoApp.swift
+//  comercio-electronico
+//
+//  Created by macOs on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct comercio_electronicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ comercio-electronicoTests/comercio_electronicoTests.swift
@@ -0,0 +1,17 @@
+//
+//  comercio_electronicoTests.swift
+//  comercio-electronicoTests
+//
+//  Created by macOs on 21/05/25.
+//
+
+import Testing
+@testable import comercio_electronico
+
+struct comercio_electronicoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
